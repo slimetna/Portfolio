1731cc80530da38898c9072e6b4c386ceea102e7
Slim <115066385+slimetna@users.noreply.github.com>
2023-01-14 11:25:07 +0100

    change translations directory

--- pages/components/contact.tsx
+++ pages/components/contact.tsx
@@ -5,7 +5,7 @@ import Swal from "sweetalert2";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useRouter } from "next/router";
-import { fr, en } from "./translations";
+import { fr, en } from "../../public/translations";
 
 export default function Contact() {
   const router = useRouter();
